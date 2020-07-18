886bef5dc3d4e7eb0814427e9a44e26ab1ca8289
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2020-07-18 15:52:57 -0700

    App Icon

---
+++ Lucid Dreams/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lucid Dreams
+//
+//  Created by Santiago Yeomans on 18/07/20.
+//  Copyright © 2020 Santiago Yeomans. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
